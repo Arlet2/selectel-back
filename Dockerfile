97a3b29c07c63ea9a18e9bc99525e3d272c6cbc1
arlet <arlet@debian-arlet.debian-arlet>
2024-02-23 02:51:01 +0300

    add test controller, fix deploy

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,12 @@
 FROM gradle:8.6.0-jdk21 AS build
 
+WORKDIR /app
+
 COPY . .
 RUN gradle bootJar
 
 FROM openjdk:21
 
-COPY --from=build /build/libs/selectel-back-0.0.1-rolling.jar /
+COPY --from=build /app/build/libs/selectel-back-0.0.1-rolling.jar /
 
 CMD java -jar selectel-back-0.0.1-rolling.jar
---
+++ src/main/kotlin/su/arlet/selectelback/controllers/TestController.kt
@@ -0,0 +1,44 @@
+package su.arlet.selectelback.controllers
+
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.ArraySchema
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.media.Schema
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class TestController {
+    @Operation(summary = "Get all deliveries")
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "200", description = "Successfully getting all deliveries",
+                content = arrayOf(
+                    Content(
+                        mediaType = "application/json",
+                        array = ArraySchema(schema = Schema(implementation = String::class))
+                    ),
+                )
+            ),
+            ApiResponse(
+                responseCode = "404", description = "No such delivery was found", content = arrayOf(Content())
+            ),
+            ApiResponse(
+                responseCode = "500", description = "Internal error", content = arrayOf(Content()),
+            )
+        ]
+    )
+    @GetMapping("\${api.path}/test")
+    @ResponseBody
+    fun getDeliveries(
+        @RequestParam(name = "transport_number", required = false) transportNumber: String?,
+        @RequestParam(name = "delivery_point_id", required = false) deliveryPointID: Int?,
+    ): List<Int> {
+        return listOf(1, 2, 3)
+    }
+}
