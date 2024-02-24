edda7da3c006324341e6c364bb5fe4bdf90048a7
arlet <shulga@niuitmo.ru>
2024-02-24 23:53:41 +0300

    fix image service

--- src/main/kotlin/su/arlet/selectelback/services/ImageService.kt
+++ src/main/kotlin/su/arlet/selectelback/services/ImageService.kt
@@ -1,7 +1,9 @@
 package su.arlet.selectelback.services
 
 import at.favre.lib.crypto.bcrypt.BCrypt
+import org.springframework.stereotype.Component
 
+@Component
 class ImageService {
     val HASH_COST = 10
 
